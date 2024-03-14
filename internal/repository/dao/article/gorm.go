217132762d0414ad0b0370cd29ba51969097a53e
lut <lut@sucsoft.com>
2024-03-14 22:28:24 +0800

    db

--- internal/repository/dao/article/gorm.go
+++ internal/repository/dao/article/gorm.go
@@ -9,14 +9,6 @@ import (
 	"time"
 )
 
-type ArticleDAO interface {
-	Insert(ctx context.Context, article Article) (int64, error)
-	Update(ctx context.Context, article Article) error
-	Sync(ctx context.Context, article Article) (int64, error)
-	Upsert(ctx context.Context, art PublishArticle) error
-	SyncStatus(ctx context.Context, id int64, authorId int64, status uint8) error
-}
-
 type GORMArticleDao struct {
 	db *gorm.DB
 }
